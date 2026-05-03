baf2c64e0b3a9fd4b8843617a1790200d7473b8c
B-bot-K <bbotkarl@gmail.com>
2026-05-03 21:18:54 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,11 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('karlbot').then(cache => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json',
-        './icon.png'
-      ]);
-    })
-  );
+self.addEventListener('install', event => {
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+self.addEventListener('activate', event => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  // simple pass-through
 });
